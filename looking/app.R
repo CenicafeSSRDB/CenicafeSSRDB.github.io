b6d6aabf2eb62b84989e1e8ab10f21807c2f08bb
Ruth Andrea Fandiño Castro <RFANDIN359@cenicafefnc.org>
2021-08-19 12:57:22 -0500

    A ver

--- looking/app.R
+++ looking/app.R
@@ -14,47 +14,42 @@ library(dplyr)
 library(readxl)
 library(DT)
 
+#Database upload
 d <- read_excel("DataBase_Markers_CENHV.xlsx")
 datos <- data.frame(Marker_ID=d$Marker_ID, Coordinates=d$Coordinates_Hv33_genome, Size=d$SSR_size,
                     Type=d$SSR_type, Forward_Primer=d$FORWARD_PRIMER1, Reverse_Primer=d$REVERSE_PRIMER1)
 
-# Define UI for application that draws a histogram
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$table <- DT::renderDataTable(DT::datatable({
+        data <-  datos[datos$Size >= input$sizes[1] & datos$Size <= input$sizes[2],]
+        if (input$types != "All") {
+            data <- data[data$Type == input$types,]
+        }
+        data 
+        }))
+}
+
+# Define UI
 ui <- fluidPage(
     titlePanel("Basic DataTable"),
+    theme = "sandstone",
     
     sidebarLayout(
         sidebarPanel(
-            selectInput("Sizes",
-                        label = "Size:", 
-                        choices = c("All", unique(as.character(datos$Size))), 
-                        selected = "All", multiple = TRUE),
-            selectInput("Types", 
-                        label = "Type:", 
-                        choices = c("All", unique(as.character(datos$Type))), 
-                        selected = "All", multiple = TRUE)
+            sliderInput("sizes", "Size:",
+                        min = 12, max = 191, value = c(12,191)),
+            
+            selectInput("types",
+                        "Type:",
+                        c("All", unique(as.character(datos$Type))),
+                        selected = "All")
         ),
         # Create a new row for the table.
         mainPanel(
             tabPanel("datos", DT::dataTableOutput("table")),
         )))
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    
-    mydata <- reactive({
-        datos %>%
-            req(input$Sizes, input$Types)
-        if(input$Sizes == "All"){
-            datos
-        }
-        if (input$Types == "All"){
-            datos
-        }
-    })
-    
-    
-    output$table <- DT::renderDataTable(DT::datatable(mydata()))
-}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
