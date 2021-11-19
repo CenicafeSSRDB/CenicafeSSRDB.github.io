febf97de1289367c94f8460cb3116c76537a2aad
Ruth Andrea Fandiño Castro <RFANDIN359@cenicafefnc.org>
2021-11-19 15:02:40 -0500

    Como una niña con el onclick

--- about/js/about.js
+++ about/js/about.js
@@ -1,5 +1,3 @@
-  var genom = document.getElementsByClassName('specieTitle');
-  function genomeChange(Gen){
-    alert("Welcome to " + Gen);
+  function message(msg){
+    document.getElementById('hv1').innerHTML = "Hemileia vastatrix - " + msg;
   }
-  genomeChange(genom);
