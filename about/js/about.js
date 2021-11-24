c7f189b3e213f02d2b55c180109bd326906458f0
Ruth Andrea Fandiño Castro <RFANDIN359@cenicafefnc.org>
2021-11-24 16:28:38 -0500

    Ya funcionana los de al aldo wuju

--- about/js/about.js
+++ about/js/about.js
@@ -1,3 +1,35 @@
   function genChange(gen){
     document.getElementById('hv1').innerHTML = "Hemileia vastatrix - " + gen;
+    genValues=document.getElementsByClassName('pag2');
+    meanContent=document.getElementsByClassName('place');
+    
+    var H1 = ["547Mb", "5469", "2%"];
+    var H2 = ["XXXMb", "XXXX", "X%"];
+    
+    var boxT = " "
+    
+    if(gen=='HV-XXXIII'){
+    
+      for (i = 0; i < genValues.length; i++){
+        boxT = genValues[i].getElementsByTagName('h1')
+        boxT[0].innerHTML = H1[i];
+      }
+      
+      var pie=document.getElementById('htmlwidget-21b323315c11bd3f6dda');
+      
+    }else if(gen=='HVCENICAFE'){
+      
+      for (i = 0; i < genValues.length; i++){
+        boxT = genValues[i].getElementsByTagName('h1')
+        boxT[0].innerHTML = H2[i];
+      }
+      
+      var pie=document.getElementById('htmlwidget-21b323315c11bd3f6dda');
+      var bars=document.getElementById('htmlwidget-6ff8b0eff05f35a17a2e');
+      
+    }
+    
   }
+ /* function initial(gen){
+    utilizar esta funcion con el onload del body, para que se carguen los datos iniciales del primer genoma
+  }*/
